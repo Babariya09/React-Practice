0991b8811205c3fb6e4d75b0c6bcfa791f9cdd7e
Babariya09 <proncebabariya38@gmail.com>
2023-09-04 15:30:00 +0530

    Page404 and Navigate Complate

--- src/Routerinreact.js
+++ src/Routerinreact.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, Link, Navigate } from 'react-router-dom'
 import Home from "./component/Home";
 import About from "./component/About";
 import Navbar from "./component/Navbar";
+import Page404 from "./component/Page404";
 
 function Routerinreact() {
     return (
@@ -16,6 +17,8 @@ function Routerinreact() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About />} />
+                    {/* <Route path="/*" element={<Page404 />} /> */}
+                    <Route path="/*" element={<Navigate to="/" />} />
                 </Routes>
             </BrowserRouter>
         </>
@@ -38,3 +41,12 @@ export default Routerinreact;
 // Anchore use in Html and link use in React.
 // tema kai pan deffrence nathi.
 // and tevu nathi ke link khali aamaj use thay tene game tya use kari shako jemke me nevbar,js ma kari che.
+
+// ================== Page404 ================
+
+// jyare url khoti hoi tyare 404 dekhadva ma te path ma nakh vanu /* etle jyare khoti hase tyare tamne aapi deche.
+
+// =============== Navigate ==================
+
+// 404 ni badle tamare khoti url per home page j dekhad vu che to navigate no use thay.
+// navigate ni ander to devanu ke tamare kya navigate karav vu che khoti url per.
