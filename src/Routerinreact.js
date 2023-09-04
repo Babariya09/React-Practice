9b182727e82a508d0dca0fe07d6d7a51f0426797
Babariya09 <proncebabariya38@gmail.com>
2023-09-04 15:16:34 +0530

    Link, navbar and anchor tag using routing

--- src/Routerinreact.js
+++ src/Routerinreact.js
@@ -1,13 +1,18 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
 import Home from "./component/Home";
 import About from "./component/About";
+import Navbar from "./component/Navbar";
 
 function Routerinreact() {
     return (
         <>
             {/* <h1>React Router 6</h1> */}
             <BrowserRouter>
+            {/* <Link to="/about">About</Link>
+            <br/><br/>
+            <Link to="/">Home</Link> */}
+            <Navbar />
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About />} />
@@ -21,6 +26,15 @@ export default Routerinreact;
 
 // ========== Note ===========
 
+// ==================== Normal Rout ====================
+
 // router nu work diffrent pages ne jodva nu hoi che jem ke navbar.
 // aa work html thi pan thay anchore na use thi but te vare vare refresh thai jay.
 // aathi router no use kari ye siye.
+
+// =================== Link, Navbar, Anchor Tag =================
+
+// link is anchores.
+// Anchore use in Html and link use in React.
+// tema kai pan deffrence nathi.
+// and tevu nathi ke link khali aamaj use thay tene game tya use kari shako jemke me nevbar,js ma kari che.
---
+++ src/component/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from 'react-router-dom'
+
+
+function Navbar() {
+    return (
+        <>
+            {/* <h1>React Router Navbar 6</h1> */}
+            {/* <Link to="/about">About</Link>
+            <br/><br/>
+            <Link to="/">Home</Link> */}
+
+            <ul>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/">Home</Link></li>
+            </ul>
+        </>
+    )
+}
+
+export default Navbar;
